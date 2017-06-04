8e3564f79c5d9c4c3a47fe9da2be19bda10fb7a2
djenkins96 <derrick.jenkins@me.com>
2017-06-04 12:16:08 -0400

    keyboard updated, lowercase keys stick if multiple keys hit at once. glyphicons added and are functional based on first character of first sentence. still need a way to cycle through characters/sentences in array 'sentences'.

--- script.js
+++ script.js
@@ -1,51 +1,84 @@
-$('#keyboard-upper-container').hide();
-//$('#keyboard-lower-container').hide();
+hideUpperCase();
 
-$(document).ready(function(){
-    $(document).keydown(function(){
-        keycode = event.keyCode;
-        //console.log(keycode);
-        //letter = String.fromCharCode(keycode)
-        //console.log(letter)
-        if (keycode === 16){
-            $('#keyboard-upper-container').show();
-            $('#keyboard-lower-container').hide();
-        } //else {
-        //     $('#keyboard-upper-container').hide();
-        //     $('#keyboard-lower-container').show()
-        // }
-    $('#'+keycode).css("background-color", "yellow")
-    
+var shft = 16;
+
+var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate']
+
+
+var expectedLetter;
+var expectedSentence;
+var mistakes;
+
+function startGame() {
+    expectedLetter = 0;
+    expectedSentence = 0;
+    mistakes = 0
+
+}
+function ready() {
+    var start = prompt('Type "start" to begin');
+    if (start = "start") {
+        startGame()
+
+    };
+};
+
+ready();
+
+function showUpperCase() {
+    $('#keyboard-upper-container').show();
+    $('#keyboard-lower-container').hide();
+}
+
+function hideUpperCase() {
+    $('#keyboard-upper-container').hide();
+    $('#keyboard-lower-container').show();
+}
+
+function highLight() {
+    $('#' + keyCode).css("background-color", "yellow");
+    $(document).keyup(function () {
+        $('#' + keyCode).css("background-color", "");
     });
-     
+};
+
+
+function animate() {
+    $("#yellow-block").finish().animate({
+        left: "+=17.5"
+    });
+}
+
+$(document).keydown(function () {
+    keycode = event.keyCode;
+    if (keycode === shft) {
+        showUpperCase();
+    }
 });
 
-$(document).ready(function(){
-    $(document).keyup(function(){
-        keycode = event.keyCode;
-        if (keycode === 16){
-            $('#keyboard-upper-container').hide();
-            $('#keyboard-lower-container').show();
-         } //else {
-        //     $('#keyboard-upper-container').show();
-        //     $('#keyboard-lower-container').hide()
-        // }
-    $('#'+keycode).css("background-color", "")
-    });
+
+$(document).keyup(function () {
+    keycode = event.keyCode;
+    if (keycode === shft) {
+        hideUpperCase();
+    }
 });
 
-$(document).ready(function(){
-     $(document).keypress(function(){
-        keycode = event.keyCode
-        letter = String.fromCharCode(keycode)
-        $('#'+keycode).css("background-color", "yellow")
-        
-        $('#sentence').text(letter);
-    });
+$(document).keypress(function () {
+    keyCode = event.keyCode;
+    var currentKey = String.fromCharCode(keyCode);
+    highLight();
+    var currentCharacter = sentences[expectedLetter][expectedSentence];
+    if (currentKey === currentCharacter) {
+        $('#feedback').append('<i class="glyphicon glyphicon-ok"></i>');
+    } else {
+        $('#feedback').append('<i class="glyphicon glyphicon-remove"></i>');
+    }
 });
-    
-        //letter = String.fromCharCode(keycode)
-        //console.log(letter)
-            
-     
+
+
+
+$('#sentence').text(sentences[0])
+$('#target-letter').text(sentences[expectedSentence][expectedLetter]);
+
 
